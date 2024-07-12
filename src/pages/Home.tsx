9e1facc1c52711ef2aca12457de2303b81b652de
LucasNeroneRillo <lneronerillo@gmail.com>
2024-07-11 22:00:30 -0500

    image and titlr

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,13 @@ import PageWrapper from '@/components/layout/PageWrapper';
 function Home() {
   return (
     <PageWrapper>
-      <div className="space-y-4 pt-60">
-        <h1 className="text-center text-5xl text-slate-300">SLICE-100K Explorer</h1>
+      <div className="space-y-4 pt-10">
+        <div className="w-100 flex items-center justify-center">
+          <img className="w-96" src="/logo.png" alt="" />
+        </div>
+        <h1 className="pb-4 text-center text-5xl text-slate-300">
+          Slice-100K Explorer
+        </h1>
         <SearchBar />
       </div>
     </PageWrapper>
